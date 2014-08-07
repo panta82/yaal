f41d332728509ae7d52be8f60b3616ebe5c2e2b0
Ivan Pantic <ivanpantic82@gmail.com>
2014-08-07 15:47:35 +0200

    Hash tested, ready for usage

--- spec/yaal.spec.js
+++ spec/yaal.spec.js
@@ -92,32 +92,60 @@ describe("Yaal", function () {
 		});
 	});
 
-	it("should respect meta switch", function (done) {
-		var startedAt = new Date(),
-			fns = [
-				libHelpers.makeTimeoutFn(100, null),
-				libHelpers.makeTimeoutFn(200, null, "11"),
-				libHelpers.makeTimeoutFn(300, null, "21", "22")
-			];
+	describe("when supplied the 'meta' switch", function () {
 
-		yaal(fns, 2, yaal.meta, function (_1, _2, meta) {
-			var completedAt = new Date();
+		it("should collect metadata in an array", function (done) {
+			var startedAt = new Date(),
+				fns = [
+					libHelpers.makeTimeoutFn(100, null),
+					libHelpers.makeTimeoutFn(200, null, "11"),
+					libHelpers.makeTimeoutFn(300, null, "21", "22")
+				];
 
-			libHelpers.expectTimestamp(expect, completedAt, startedAt.getTime() + 400);
+			yaal(fns, 2, yaal.meta, function (_1, _2, meta) {
+				var completedAt = new Date();
 
-			libHelpers.expectTimestamp(expect, meta.startedAt, startedAt);
-			libHelpers.expectTimestamp(expect, meta.completedAt, completedAt);
+				libHelpers.expectTimestamp(expect, completedAt, startedAt.getTime() + 400);
 
-			libHelpers.expectTimestamp(expect, meta[0].startedAt, startedAt);
+				libHelpers.expectTimestamp(expect, meta.startedAt, startedAt);
+				libHelpers.expectTimestamp(expect, meta.completedAt, completedAt);
 
+				libHelpers.expectTimestamp(expect, meta[0].startedAt, startedAt);
+				libHelpers.expectTimestamp(expect, meta[0].completedAt, startedAt.getTime() + 100);
+				libHelpers.expectTimestamp(expect, meta[1].startedAt, startedAt);
+				libHelpers.expectTimestamp(expect, meta[1].completedAt, startedAt.getTime() + 200);
+				libHelpers.expectTimestamp(expect, meta[2].startedAt, startedAt.getTime() + 100);
+				libHelpers.expectTimestamp(expect, meta[2].completedAt, startedAt.getTime() + 400);
 
-			libHelpers.expectTimestamp(expect, meta[0].completedAt, startedAt.getTime() + 100);
-			libHelpers.expectTimestamp(expect, meta[1].startedAt, startedAt);
-			libHelpers.expectTimestamp(expect, meta[1].completedAt, startedAt.getTime() + 200);
-			libHelpers.expectTimestamp(expect, meta[2].startedAt, startedAt.getTime() + 100);
-			libHelpers.expectTimestamp(expect, meta[2].completedAt, startedAt.getTime() + 400);
+				done();
+			});
+		});
 
-			done();
+		it("should collect metadata in a hash", function (done) {
+			var startedAt = new Date(),
+				fns = {
+					"A": libHelpers.makeTimeoutFn(100, null),
+					"B": libHelpers.makeTimeoutFn(200, null, "B1"),
+					3: libHelpers.makeTimeoutFn(300, null, "31", "32")
+				};
+
+			yaal(fns, 3, yaal.meta, function (_1, _2, meta) {
+				var completedAt = new Date();
+
+				libHelpers.expectTimestamp(expect, completedAt, startedAt.getTime() + 300);
+
+				libHelpers.expectTimestamp(expect, meta.startedAt, startedAt);
+				libHelpers.expectTimestamp(expect, meta.completedAt, completedAt);
+
+				libHelpers.expectTimestamp(expect, meta["A"].startedAt, startedAt);
+				libHelpers.expectTimestamp(expect, meta["A"].completedAt, startedAt.getTime() + 100);
+				libHelpers.expectTimestamp(expect, meta["B"].startedAt, startedAt);
+				libHelpers.expectTimestamp(expect, meta["B"].completedAt, startedAt.getTime() + 200);
+				libHelpers.expectTimestamp(expect, meta["3"].startedAt, startedAt);
+				libHelpers.expectTimestamp(expect, meta["3"].completedAt, startedAt.getTime() + 300);
+
+				done();
+			});
 		});
 	});
 
