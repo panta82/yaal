196d53ce366381b199159ee3b4ac069f46e86b8b
Ivan Pantic <ivanpantic82@gmail.com>
2014-08-20 01:01:32 +0200

    First hash switch tests

--- spec/yaal.spec.js
+++ spec/yaal.spec.js
@@ -316,6 +316,59 @@ describe("Yaal", function () {
 		});
 	});
 
+	describe("when supplied the 'hash' switch", function () {
+
+		describe("in a task-for-each scenario", function () {
+
+			it("can map list of arguments into a hash", function (done) {
+				yaal(libHelpers.asyncToUppercase, ["a", "b"], "hash", function (err, res) {
+					expect(err).toBe(null);
+					expect(res.count).toBe(2);
+					expect(res["a"]).toBe("A");
+					expect(res["b"]).toBe("B");
+					done();
+				});
+			});
+
+			it("can extract meaningful keys from non-string arguments", function (done) {
+				var dateStr = "Wed Aug 20 2014 00:57:24 GMT+0200 (CEST)",
+					args = ["str", 3, {x: "no"}, {id: "yes"}, [], false, true, /^regex$/, new Date(dateStr), null, undefined];
+				yaal(libHelpers.asyncToUppercase, args, "hash", function (err, res) {
+					expect(err).toBe(null);
+					expect(res.count).toBe(8);
+					expect(res["str"]).not.toBe(undefined);
+					expect(res["3"]).not.toBe(undefined);
+					expect(res["[object Object]"]).not.toBe(undefined);
+					expect(res["yes"]).not.toBe(undefined);
+					expect(res["true"]).not.toBe(undefined);
+					expect(res["false"]).not.toBe(undefined);
+					expect(res["/^regex$/"]).not.toBe(undefined);
+					expect(res[dateStr]).not.toBe(undefined);
+					expect(res["null"]).toBe(undefined);
+					expect(res["undefined"]).toBe(undefined);
+					done();
+				});
+			});
+		});
+
+		describe("in a list-of-tasks scenario", function () {
+
+			it("can map task names into a hash", function (done) {
+				var fns = [
+					function a(cb) { cb(null, "A") },
+					function b(cb) { cb(null, "B") }
+				];
+				yaal(fns, "hash", function (err, res) {
+					expect(err).toBe(null);
+					expect(res.count).toBe(2);
+					expect(res["a"]).toBe("A");
+					expect(res["b"]).toBe("B");
+					done();
+				});
+			});
+		});
+	});
+
 	describe("if given multiple switches", function () {
 
 		it("can parse them as individual arguments", function (done) {
